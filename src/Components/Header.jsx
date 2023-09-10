46db9197b9adad79a17395196800c326c45a75de
sarthakshrm-dev <sarthakshrm2001@gmail.com>
2023-09-11 03:04:09 +0530

    final updates

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,17 +2,12 @@ import React from 'react';
 
 function Header() {
   return (
-    // <header>
-    //   <h1>Shree Retina Care</h1>
-    // </header>
-
     <header>
-	<div class="overlay">
-<h1>Shree Retina Care</h1>
-<h3>Thanks for Choosing US</h3>
-		</div>
-</header>
-
+      <div class="overlay">
+        <h1>Shree Retina Care</h1>
+        <h3>Thanks for Choosing US</h3>
+      </div>
+    </header>
   );
 }
 
